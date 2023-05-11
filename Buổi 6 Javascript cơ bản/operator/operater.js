3d4bc600ec74aedf5d2ffcda8be2a7551ae5b281
hieu16129 <infor.duchieu.1999@gmail.com>
2023-05-11 16:06:15 +0700

    bai1

--- Buổi 6 Javascript cơ bản/operator/operater.js
+++ Buổi 6 Javascript cơ bản/operator/operater.js
@@ -5,4 +5,4 @@ inputHeight = prompt("enter the height");
 let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
 let area = width * height;
-document.write("Area = " +area);
+document.write("Area = " +area)
